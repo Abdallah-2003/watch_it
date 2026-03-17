ee9f865de197402ead1d62fcd17c8818d83c327e
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-03-17 17:28:42 +0200

    layout bottom navigation bar states and cubit done

---
+++ lib/features/layout/presentation/cubit/layout_cubit/layout_cubit.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:watch_it/home_view.dart';
+
+part 'layout_state.dart';
+
+class LayoutCubit extends Cubit<LayoutState> {
+  LayoutCubit() : super(LayoutInitial());
+
+  int currentIndex = 0;
+
+  List<Widget> views = [
+    HomeView(),
+    Container(color: Colors.red,)
+  ];
+
+  void changeCurrentIndex(int index) {
+    currentIndex = index;
+    emit(LayoutChange());
+  }
+}
