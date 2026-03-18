dadec27a0b9108e83b1c1e2898c171e88a159d88
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-03-18 14:36:10 +0200

    remove descrption from videeo table and video model

--- lib/features/home/data/model/video_model.dart
+++ lib/features/home/data/model/video_model.dart
@@ -2,14 +2,12 @@ class VideoModel {
 
   final String id;
   final String title;
-  final String desc;
   final String videoUrl;
   final String videoImageUrl;
 
   const VideoModel({
     required this.title,
     required this.id,
-    required this.desc,
     required this.videoImageUrl,
     required this.videoUrl,
   });
@@ -18,7 +16,6 @@ class VideoModel {
     return VideoModel(
       id: json['id'],
       title: json['title'],
-      desc: json['desc'],
       videoUrl: json['video_url'],
       videoImageUrl: json['video_image_url'],
     );
